bb47b8be9c671941718cfa71ff7bfe39699eb60a
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-09-20 16:15:16 +0300

    Сделал

--- js/scripts.js
+++ js/scripts.js
@@ -15,48 +15,5 @@ $(() => {
 	    })
 	})
 
-
-  
-	$("form").submit(function() {
-        // Получение ID формы
-        var formID = $(this).attr('id');
-        // Добавление решётки к имени ID
-        var formNm = document.getElementById(formID);
-        $.ajax({
-            type: "POST",
-            url: 'send.php',
-            data: new FormData(formNm),
-            processData: false,
-            contentType: false,
-            beforeSend: function() {
-                // Вывод текста в процессе отправки
-                //$(formNm).html('<p style="text-align:center">Отправка...</p>');
-            },
-            success: function(data) {
-                // Вывод текста результата отправки
-                //$(formNm).html('<p style="text-align:center">' + data + '</p>');
-
-			    $.fancybox.open({
-			        src: "#callback__modal2",
-			        type: 'inline',
-			        touch: false
-			    })
-			    $(formNm).trigger('reset');
-
-            },
-            error: function(jqXHR, text, error) {
-                // Вывод текста ошибки отправки
-                //$(formNm).html(error);
-            }
-        });
-        return false;
-    });
-
-
 })
-
-
-function handleFiles(file) {
-    const fileList = file;
-    $(".upload-file__text").text(fileList[0]["name"]);
-}
+  
